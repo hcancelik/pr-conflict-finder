965e108a07848bb1a742ce9bed36993bab537a67
Can Celik <celik@me.com>
2021-02-08 21:43:46 +0300

    test

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ async function run() {
 
     if (github.payload && github.payload.pull_request) {
       pr = github.payload.pull_request;
+
+      core.info(JSON.stringify(github.payload));
+      core.info(JSON.stringify(github));
+
+      // core.info("Skipping check because head is being merged");
     }
 
     const action = new Action(token, owner, repo, label, maxTries, waitMs);
