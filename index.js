20c662d901da46e4d07c7e0b01cb226a93a0f4cd
Can Celik <celik@me.com>
2022-04-09 11:17:34 +0300

    Another test

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const Action = require("./src/action");
 async function run() {
   try {
     const token = core.getInput("SECRET_TOKEN");
+    core.info(`Test: ${token}`);
+
     const label = core.getInput("CONFLICT_LABEL");
     const maxTries = Number(core.getInput("MAX_TRIES"));
     const waitMs = Number(core.getInput("WAIT_MS"));
 
     const { owner, repo } = github.repo;
+
     console.info(token === null ? "No token provided" : "Token provided");
 
     let pr = null;
